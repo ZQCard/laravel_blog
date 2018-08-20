4376fa36f8ed3e93523f93206d164a7fc73ced62
445864742@qq.com <445864742@qq.com>
2018-08-20 11:46:22 +0800

    将标签处理放在后置方法中

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -29,13 +29,13 @@ class ArticleController extends AdminController
         $request_data = $request->all();
         $article->fill($request->all());
         // 把标签信息存如session，在文章观察器中拉取
-        $request->session()->put('article.keywords', $request_data['other_tags']);
+        $request->session()->flash('article.keywords', $request_data['other_tags']);
+        // 将标签取出来after save处理
+        $tags_data['tag_ids'] = $request_data['tag_ids'];
+        $tags_data['other_tags'] = $request_data['other_tags'];
+        $request->session()->flash('article.tags', $tags_data);
         // 保存文章
         if ($article->save()){
-            // 将标签取出来另外处理
-            $tags_data['tag_ids'] = $request_data['tag_ids'];
-            $tags_data['other_tags'] = $request_data['other_tags'];
-            (new ArticleTag)->articleRelateTag($tags_data, $article->id);
             return $this->success('创建文章成功', route('article.index'));
         }
         return $this->fail('数据库保存失败');
--- app/Observers/ArticleObserver.php
+++ app/Observers/ArticleObserver.php
@@ -3,13 +3,27 @@
 namespace App\Observers;
 
 use App\Models\Article;
+use App\Models\ArticleTag;
 
 class ArticleObserver
 {
+    /**
+     * 前置方法 before insert|before update
+     * @param Article $article
+     */
     public function saving(Article $article)
     {
         $article->score = time();
         $article->keywords = config("personal.keywords").','.session('article.keywords');
         $article->excerpt = make_excerpt($article->content);
+    }
+
+    /**
+     * 后置方法 after insert|after update
+     * @param Article $article
+     */
+    public function saved(Article $article)
+    {
+        (new ArticleTag)->articleRelateTag(session('article.tags'), $article->id);
     }
 }
