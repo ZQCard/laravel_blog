4e5f28da1fba93c0fc762796cfc3fd765b0de268
445864742@qq.com <445864742@qq.com>
2018-08-27 13:55:43 +0800

    修改登陆样式

--- resources/views/home/layouts/app.blade.php
+++ resources/views/home/layouts/app.blade.php
@@ -45,6 +45,7 @@
                         @guest
                             <li><a href="{{ route('login') }}">登陆</a></li>
                             <li><a href="{{ route('register') }}">注册</a></li>
+                            <li><a href="{{ route('home') }}">首页</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
