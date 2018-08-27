19ede796cc9a6a0c44ce7ce627e57e6c2fa5bf2d
ZQCard <445864742@qq.com>
2018-08-27 23:19:13 +0800

    添加评论表以及假数据

--- database/factories/ArticleCommentFactory.php
+++ database/factories/ArticleCommentFactory.php
@@ -1,7 +1,7 @@
 <?php
 
 use Faker\Generator as Faker;
-use Carbon\Carbon;
+
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -13,16 +13,17 @@ use Carbon\Carbon;
 |
 */
 
+$factory->define(App\Models\ArticleComment::class, function (Faker $faker) {
 
-$factory->define(App\Models\User::class, function (Faker $faker) {
-    static $password;
-    $now = Carbon::now()->toDateTimeString();
+    // 随机取一个月以内的时间
+    $updated_at = $faker->dateTimeThisMonth();
+    // 传参为生成最大时间不超过，创建时间永远比更改时间要早
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
+
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('password'),
-        'remember_token' => str_random(10),
-        'created_at' => $now,
-        'updated_at' => $now,
+        'content' => $faker->text(),
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
     ];
 });
