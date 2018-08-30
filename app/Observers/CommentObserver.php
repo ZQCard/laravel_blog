0fc454ffed41fb7dd88523e16306deffe5ba59b6
zhouqi445864742 <445864742@qq.com>
2018-08-30 21:01:59 +0800

    加入redis异步队列

--- app/Observers/CommentObserver.php
+++ app/Observers/CommentObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Handlers\Email;
+use App\Jobs\SendReminderEmail;
 use App\Models\Article;
 use App\Models\ArticleComment;
 use App\Models\ArticleTag;
@@ -16,25 +17,7 @@ class CommentObserver
      */
     public function saved(ArticleComment $comment)
     {
-        // 文章信息
-        $article['title'] = $comment->article->title;
-        $article['id'] = $comment->article->id;
-
-        // 当前评论用户
-        $current_data['user'] = $comment->user->name;
-        $current_data['comment'] = $comment->content;
-        // 通知管理员
-        Email::commentToAdmin($article, $current_data);
-
-        if ($comment->parent_id != 0){
-            // 之前评论用户
-            $original = ArticleComment::find($comment->parent_id);
-            $original_data['user'] = $original->user->name;
-            $original_data['comment'] = $original->content;
-            $original_data['email'] = $original->user->email;
-            // 通知原评用户
-            Email::commentToUser($article, $current_data, $original_data);
-        }
+        dispatch(new SendReminderEmail($comment));
     }
 
 
