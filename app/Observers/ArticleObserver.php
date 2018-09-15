04271e7162ac78df372271b9d7899ffac0dcc25d
ZQCard <445864742@qq.com>
2018-09-16 02:17:43 +0800

    编辑文档后删除标签云缓存

--- app/Observers/ArticleObserver.php
+++ app/Observers/ArticleObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Models\Article;
 use App\Models\ArticleTag;
+use Illuminate\Support\Facades\Redis;
 
 class ArticleObserver
 {
@@ -25,6 +26,8 @@ class ArticleObserver
     public function saved(Article $article)
     {
         (new ArticleTag)->articleRelateTag(session('article.tags'), $article->id);
+        // 清除标签缓存
+        Redis::Del('common.tags');
     }
 
 
